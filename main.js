031ff7c0e53aaa1322d546a0848742fdd2535cc4
Niveditha998 <94441597+Niveditha998@users.noreply.github.com>
2025-07-16 19:05:43 -0400

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new typed(".text", {
-    String: ["Frontend Developer","Graduate Student","Web Developer" , "Full Stack Developer" ],
-    typeSpeed: 100,
-    backspeed:100,
-    backDelay:1000,
-    loop : true
-})
